3e686edbd19b90f5bf55de37443b675cd2912d67
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-08 18:36:59 +0100

    Setup the the index file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// import store from './redux/store';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', //  http://localhost:4000/graphql https://flyby-gateway.herokuapp.com/
+  cache: new InMemoryCache(),
+});
+
+
+const append = document.querySelector('#root');
+const root = createRoot(append);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      {/* <Provider store={store}> */}
+      <ApolloProvider client={client}>
+        <App />
+        </ApolloProvider>
+      {/* </Provider> */}
+    </Router>
+  </React.StrictMode>,
+);
