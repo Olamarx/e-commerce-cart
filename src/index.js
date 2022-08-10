0a569513147842fa93029f33bfb23eb7126a3ae6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-10 01:20:49 +0100

    Restructured the index file.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import App from './components/App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "http://localhost:4000",
   cache: new InMemoryCache()
 });
-
 
 const append = document.querySelector('#root');
 const root = createRoot(append);
