b91d5a09f1392a061ada38da4c494f17ec8e62e6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-10 01:21:23 +0100

    Added query for the UI usage

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import { gql } from '@apollo/client';
-const getData = gql`
-   query {
-    categories {
-    products
-    }
-  }
-`;
+// import API  from './API';
+import { Query } from '@apollo/client/react/components';
+import  { getAllProducts } from '../graphql/queries';
+
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = []
   };
 
   componentDidMount() {
-    this.setState(getData)
-  }
+    <Query query={getAllProducts}>
 
+    </Query>
+  }
+    
   render() {
     return (
-      <div>{ console.log(this.state) } App</div>
-    )
+      <div className="App">
+        <Query query={getAllProducts}>
+        {({ loading, error, data }) => {
+                if (loading) return <h2>Loading..</h2>;
+                if (error) return <h2>Error: error</h2>;
+                if (data) {
+                  // destructuring data
+                  const { products } = data.category;
+                  return (
+                    <div>
+                      {products.map((product) => (
+                    <div key={product.id}>
+                      {product.gallery}
+                    </div>
+                      ))}
+                    </div>
+                  );
+                }
+              }}
+        </Query>
+      </div>
+    );
   }
 }
