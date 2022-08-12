a30800d11cf2deba7d92f39b3e30374454d827ef
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-12 09:38:37 +0100

    Added routes fpr the pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Men from './Pages/men/Men';
 import Kids from './Pages/Kids/Kids';
+import Women from './Pages/women/Women';
+import Navigation from './Navigation/Navigation';
+import './App.css';
+
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Routes>
-        <Route path="/" element={<Navigate to="/women" />} /> 
-        <Route path="/men/" element={<Men />} /> 
-        <Route path="/kids/" element={<Kids/>} />
-        </Routes> 
-      </div>
+        <div>
+          <Navigation />
+          <Routes>
+          <Route path="/" exact element={<Women />} /> 
+          <Route path="/men" element={<Men />} /> 
+          <Route path="/kids" element={<Kids/>} />
+          </Routes> 
+        </div>
     )
   }
 }
