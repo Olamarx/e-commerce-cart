aec6fc0257bf5fe4d37502d723f033f1ba865eb6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-12 09:39:20 +0100

    Added the currency but not yet fully functional

---
+++ src/components/Navigation/Currencies.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Query } from '@apollo/client/react/components';
+import { getCurrencies } from '../../graphql/queries';
+
+export default class Currencies extends Component {
+constructor(props){
+  super(props)
+  this.state = { symbol: '$' }
+
+  this.handleChange = this.handleChange.bind(this)
+}
+
+handleChange(e) {
+  this.setState({symbol: e.target.value});
+}
+
+  render() {
+    return (
+        <>
+        <Query query={getCurrencies}>
+        {({ data }) => {
+          if (data) {
+            const { currencies } = data
+            console.log(currencies);
+            <select
+            value={this.state.symbol}
+            onChange={this.handleChange}
+            >
+                  <option value={this.state.symbol}>{null}</option>
+                    {
+                      currencies.map(({ symbol, label }) => (
+                        <option
+                        value={symbol}
+                        key={symbol}
+                        >
+                          gdfgdg
+                          {label}
+                        </option>
+                      ))
+                    }
+                  </select>
+                }
+        }}
+      </Query>
+      </>
+    )
+  }
+}
