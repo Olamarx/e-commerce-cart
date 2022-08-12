f6dcf7001dc699a7361a488f46bfb5979863e9e5
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-12 09:36:47 +0100

    Added redux provide to access the store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 // import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducer from './redux/configfile';
+
+const store = createStore(reducer)
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -18,7 +23,9 @@ root.render(
     <Router>
       {/* <Provider store={store}> */}
       <ApolloProvider client={client}>
+      <Provider store={store}>
         <App />
+      </Provider>
         </ApolloProvider>
       {/* </Provider> */}
     </Router>
