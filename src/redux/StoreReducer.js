66d21d259fc7ea103ce59dbfa09ff3ddc44bfb1a
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-14 12:38:28 +0100

    the product reducer to add to the cart

--- src/redux/StoreReducer.js
+++ src/redux/StoreReducer.js
@@ -1,122 +1,149 @@
-export const cartTypes = {
-    CARTEGORIES: 'cart/products/CATEGORIES',
-    CARTEGORY: 'cart/products/CATEGORY',
-    PRODUCT: 'cart/product/PRODUCT',
-    CURRENCIES: 'cart/product/CURRENCIES'
+export const Action = {
+    ADD_PRODUCT: 'cart/product/REMOVE_PRODUCT',
+    REMOVE_PRODUCT: 'cart/product/REMOVE_PRODUCT',
 }
 
-export const GetCategories = (category) =>({
-    type: cartTypes.CARTEGORY,
-    payload: { category }
-})
-
-
-
-// metrics-webapp, manel
-
-// export const GetCategory = () => {
-//     const theQuery = `
-//     query getProducts {
-//         category {
-//           name
-//           products {
-//             name
-//             inStock
-//             category
-//             brand
-//             id
-//             gallery
-//             attributes {
-//               id
-//               name
-//               type
-//               items {
-//                 id
-//                 value
-//                 displayValue
-//               }
-//             }
-//             prices {
-//               currency{
-//                 label
-//                 symbol
-//               }
-//               amount
-//             }
-//           }
-//         }
-//       }
-//     `;
-//     fetch("http://localhost:4000", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json",
-//         },
-//         body: JSON.stringify({
-//             query: theQuery
-//         })
-//     }).then(response => {
-//         return response.json();
-//     }).then(data => {
-//         console.log(data)
-//         return data
-//     });
-// }
-
-
-
-export const GetProduct = async (productsData) => {
-
-
-    return {
-        type: cartTypes.PRODUCT,
-        payload: { productsData }
+const theQuery = `
+query allCategories {
+    categories{
+      name
+      products{
+        id
+          name
+        inStock
+        gallery
+        description
+        category
+        attributes{
+          id
+          name
+          type
+          items{
+            displayValue
+            value
+            id
+          }
+        }
+        prices{
+          currency{
+            label
+            symbol
+          }
+          amount        
+        }
+        brand
+        
+      }
     }
+  }
+`;
 
+// reducer
+const productReducer = (state = [], action) => {
+    const { payload } = action;
+    switch (action.type){
+        case Action.ADD_PRODUCT:
+            return [...state, payload.products];
+        case Action.REMOVE_PRODUCT:
+            state.filter((product) => product.id !== payload.id);
+            break;
+        default:
+            return state
+    }
 }
 
-export const GetCurrencies = (currencies) => ({
-    type: cartTypes.CURRENCIES,
-    payload: { currencies }
-})
-  
-  const initialize = {
-    //   categories: [],
-    //   category: [],
-      product: [],
-    //   currencies: []
-    };
-
-const productReducer = (state = initialize, action ) => {
-    // switch(action.type) {
-        // case cartTypes.PRODUCT:
-            console.log(cartTypes.PRODUCT, action)
-    // }
-    return state
+// Action Creator 
+// dispatch, name
+const addProduct = () => {
+    fetch("http://localhost:4000", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                query: theQuery
+            })
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            const { categories } = data.data
+            console.log(categories[0]);
+            return categories
+            // const eachProcduct = categories.filter((product) => product.name === name)
+        // dispatch({type: Action.ADD_PRODUCT, payload: eachProcduct})
+        });
 }
 
-    // if (action.type === cartTypes.PRODUCT) {
-    //     let newState = GetCategory()
-    //     return { ...state, 
-    //     product: newState
-    //     }
-    // const { payload } = action
-    // switch (action.type) {
-        // case cartTypes.CARTEGORIES:
-        //     return { ...state  };
-        //     break;
-        // case cartTypes.CARTEGORY:
-        //         return { ...payload }; 
-        //     break;
-        // case cartTypes.PRODUCT:
-        //         return { ...state,  };
-        //         break;
-            // case cartTypes.CURRENCIES:
-            //     return { ...state };
-        // default:
-        //     return state;
-    // }
+export {
+    addProduct,
+}
+
+export default productReducer
+
+// export const GetCategories = (category) =>({
+//     type: cartTypes.CARTEGORY,
+//     payload: { category }
+// })
+
+// export const GetProduct = async (productsData) => {
+
+
+//     return {
+//         type: cartTypes.PRODUCT,
+//         payload: { productsData }
+//     }
+
 // }
 
-export default productReducer;
+// export const GetCurrencies = (currencies) => ({
+//     type: cartTypes.CURRENCIES,
+//     payload: { currencies }
+// })
+  
+//   const initialize = {
+//     //   categories: [],
+//     //   category: [],
+//       product: [],
+//     //   currencies: []
+//     };
+
+// const productReducer = (state = initialize, action ) => {
+//     // switch(action.type) {
+//         // case cartTypes.PRODUCT:
+//             console.log(cartTypes.PRODUCT, action)
+//     // }
+//     return state
+// }
+
+//     // if (action.type === cartTypes.PRODUCT) {
+//     //     let newState = GetCategory()
+//     //     return { ...state, 
+//     //     product: newState
+//     //     }
+//     // const { payload } = action
+//     // switch (action.type) {
+//         // case cartTypes.CARTEGORIES:
+//         //     return { ...state  };
+//         //     break;
+//         // case cartTypes.CARTEGORY:
+//         //         return { ...payload }; 
+//         //     break;
+//         // case cartTypes.PRODUCT:
+//         //         return { ...state,  };
+//         //         break;
+//             // case cartTypes.CURRENCIES:
+//             //     return { ...state };
+//         // default:
+//         //     return state;
+//     // }
+// // }
+
+// export default productReducer;
+
+// // CARTEGORY: 'cart/products/CATEGORY',
+// PRODUCT: 'cart/product/PRODUCT',
+// CURRENCIES: 'cart/product/CURRENCIES'
