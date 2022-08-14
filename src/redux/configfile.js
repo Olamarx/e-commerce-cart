9a6518f614b29c6a15be1cc0d7c159e564905ad0
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-14 04:50:50 +0100

    the combine reducer

--- src/redux/configfile.js
+++ src/redux/configfile.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import reducer from './StoreReducer';
+import productReducer from './StoreReducer';
 
 
 export default combineReducers({
-    cart: reducer
+    product: productReducer
 })
