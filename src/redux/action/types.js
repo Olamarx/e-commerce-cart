c4308c3aca207b5c1010fb2c5bc1b531d4ec3137
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-16 11:43:45 +0100

    added redux types according to Ducks pattern

---
+++ src/redux/action/types.js
@@ -0,0 +1,3 @@
+export const FETCH_PRODUCTS = "products/products/FETCH_PRODUCTS"
+export const FILTER_PRODUCTS = "filter/products/FILTER_PRODUCTS"
+export const ADD_TO_CART = "add/products/ADD_TO_CART"
