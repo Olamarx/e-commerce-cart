7239f26110cfce0f936cf5cc928cf887c4d8ef26
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-16 11:19:57 +0100

    A lot is is still to be refactored but I fixed the filter and the redering of the data on the UI now

--- src/components/Pages/women/Women.js
+++ src/components/Pages/women/Women.js
@@ -1,46 +1,116 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-// import { Query } from '@apollo/client/react/components';
-// import  { getAllProducts } from '../../../graphql/queries';
-// import { centered, imageContainer } from './helper';
+import { theQuery } from '../../../redux/StoreReducer';
 import './women.css'
-import { cartTypes, GetCategory } from '../../../redux/StoreReducer'
-import productFromAPI from '../../../redux/action/productAction';
+import Products from './Products';
+import Filter from './Filter'
 
-class Women extends Component {
-  // const categories = getState('categories')
-//  constructor(props){
-//   super(props)
-//   this.props.pr
-// }
+export default class Women extends Component {
+  constructor(props){
+    super(props);
+    this.state = { products: [], filteredProducts: [] }
+    this.handleChangeCategory = this.handleChangeCategory.bind(this)
+  }
+
+  componentDidMount() {
+      fetch("http://localhost:4000", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+              },
+              body: JSON.stringify({
+                  query: theQuery
+              })
+          })
+          .then(response => {
+              return response.json();
+          })
+          .then(data => {
+            const { categories } = data.data
+            this.setState({
+              products: categories[0].products,
+              filteredProducts: categories[0].products,
+            })
+          });  
+  }
   
+handleChangeCategory(e){
+  this.setState({sort: e.target.value})
+  this.listProducts()
+}
+listProducts(){
+  this.setState(state => {
+    if (state.sort === "tech"){
+      const filt = state.products.filter(product => product.category === "tech")
+      return {filteredProducts: filt }
+    } else if (state.sort === "clothes") {
+      const filt = state.products.filter(product => product.category === "clothes")
+      return {filteredProducts: filt }
+    }
+    else return { filteredProducts: state.products }
+  })
+}
+
   render() {
-    // GetCategory()
-    // console.log(this.props)
-    const { products } = this.props
-    return (
-      <div> Working </div>
+    // console.log(this.state)
+  return (
+    <>
+    <Filter
+    sort={this.state.sort}
+    handleChangeCategory={this.handleChangeCategory}
+    />
+    <Products
+    allProducts={this.state.filteredProducts}
+    handleAddToCart={this.handleAddToCart}
+    />
+    </>
     );
   }
-}
 
-
-const mapStateToProps = (state) => {
-  return {
-    products: state.product.product
+    // const { products } = this.props
+    
   }
-}
+    // <>
+    // {/* <Filter categories/>
+    // {/* <button onClick={() => categories[1] ? categories[1] : '' }>Clothes</button> */}
+    // {/* <section className="container">
+    //   {this.state.all.products.map((product) => (
+    // <article className="card" key={product.id}>
+    //   <div style={imageContainer}>
+    //   <img className="card_image" src={product.gallery[0]} alt={product.name} />
+    //     {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
+    //   </div>
+    //   <div>
+    //   <div>{product.name}</div>
+    //   <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
+    //   </div>
+    // </article>
+    //   ))}
+    // </section>
+    //   */}
 
-const mapDispatchToProps =(dispatch) => {
-      return {
-        // categories: () => dispatch({ type: cartTypes.CARTEGORIES }),
-        // category: () => dispatch({ type: cartTypes.CARTEGORY }),
-        product: () => dispatch(productFromAPI())
-        // currencies: () => dispatch({ type: cartTypes.CURRENCIES })
-      }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Women)
+// const mapStateToProps = (state) => {
+//   return {
+//     cart: state.cart
+//   }
+// }
+
+// const mapDispatchToProps =(dispatch) => {
+//       return {
+//         Product: dispatch(addProduct)
+//         // categories: (product) => dispatch({ type: Action.ADD_PRODUCT, payload: product }),
+//       }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Women)
+
+
+// //   componentDidMount(){
+// // this.props.dispatch({
+//   payload: addProduct
+// })
+// }
 
 
 
