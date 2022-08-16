afb729cfc4b4a121e8e19c59f920a5a3782022ea
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-16 11:13:56 +0100

    Refactored the file and removed a number of unnecessary comments and line of code.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
 import App from './components/App';
-import reducer from './redux/combineReducer';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger'
-// import { GetCategory } from './redux/StoreReducer';
-
-// GetCategory()
-
-const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -27,9 +16,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <ApolloProvider client={client}>
-      <Provider store={store}>
         <App />
-      </Provider>
         </ApolloProvider>
     </Router>
   </React.StrictMode>,
