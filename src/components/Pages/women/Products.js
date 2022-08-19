0cb2e110f73d68c301416985ab7633f604061104
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-19 16:05:11 +0100

    added the reducer but yet to be functioning

--- src/components/Pages/women/Products.js
+++ src/components/Pages/women/Products.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { imageContainer, centered } from './helper'
+import { connect } from 'react-redux'
+import './products.css'
+import addProductImage from "../../../utils/Common.png"
+import {fetchProducts } from '../../../redux/action/productActions'
+class Products extends Component {
 
-export default class Products extends Component {
+  componentWillMount() {
+    this.props.fetchProducts()
+  }
 
   render() {
-    console.log(this.props.allProducts)
-    const productItems = this.props.allProducts.map(product => (
+    console.log(this.props.products)
+    const productItems = this.props.products.map(product => (
             <article className="card" key={product.id}>
                 <div style={imageContainer}>
                     <img className="card_image" src={product.gallery[0]} alt={product.name} />
+                    {product.inStock ? (<img src={addProductImage} alt={product.name}
+                    style={{cursor: "pointer"}}
+                    onClick={(e) => this.props.handleAddToCart(e, product)} />)
+                    : null}
                     {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
                 </div>
                 <div>
@@ -24,3 +35,6 @@ export default class Products extends Component {
     }  
 }
 
+const mapStateToProps = state => ({products: state.products})
+
+export default connect(mapStateToProps, {fetchProducts})(Products)
