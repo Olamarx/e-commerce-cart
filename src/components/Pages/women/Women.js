861834b620729f04632a0490d4e0b046aa0cdedb
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-19 16:14:27 +0100

    added some functions but trying to set it in the redux so it will dispatch to the redux and get products from the redux

--- src/components/Pages/women/Women.js
+++ src/components/Pages/women/Women.js
@@ -1,44 +1,49 @@
 import React, { Component } from 'react';
 import { theQuery } from '../../../redux/StoreReducer';
-import './women.css'
 import Products from './Products';
 import Filter from './Filter'
-
+import Basket from './Basket';
+import "./women.css"
 export default class Women extends Component {
   constructor(props){
     super(props);
-    this.state = { products: [], filteredProducts: [] }
-    this.handleChangeCategory = this.handleChangeCategory.bind(this)
+    this.state = { products: [], filteredProducts: [], cartItems: [] }
+    // this.handleChangeCategory = this.handleChangeCategory.bind(this)
+    // this.handleAddToCart = this.handleAddToCart.bind(this)
+    // this.increaseNumber = this.increaseNumber.bind(this)
+    // this.decreaseNumber = this.decreaseNumber.bind(this)
   }
 
-  componentDidMount() {
-      fetch("http://localhost:4000", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-                  "Accept": "application/json",
-              },
-              body: JSON.stringify({
-                  query: theQuery
-              })
-          })
-          .then(response => {
-              return response.json();
-          })
-          .then(data => {
-            const { categories } = data.data
-            this.setState({
-              products: categories[0].products,
-              filteredProducts: categories[0].products,
-            })
-          });  
-  }
+
+
+  // componentDidMount() {
+  //     fetch("http://localhost:4000", {
+  //             method: "POST",
+  //             headers: {
+  //                 "Content-Type": "application/json",
+  //                 "Accept": "application/json",
+  //             },
+  //             body: JSON.stringify({
+  //                 query: theQuery
+  //             })
+  //         })
+  //         .then(response => {
+  //             return response.json();
+  //         })
+  //         .then(data => {
+  //           const { categories } = data.data
+  //           this.setState({
+  //             products: categories[0].products,
+  //             filteredProducts: categories[0].products,
+  //           })
+  //         });  
+  // }
   
-handleChangeCategory(e){
+handleChangeCategory = (e) => {
   this.setState({sort: e.target.value})
   this.listProducts()
 }
-listProducts(){
+listProducts = () => {
   this.setState(state => {
     if (state.sort === "tech"){
       const filt = state.products.filter(product => product.category === "tech")
@@ -51,96 +56,67 @@ listProducts(){
   })
 }
 
+handleAddToCart = (e, product) => {
+  this.setState((state) => {
+    const cartItems = state.cartItems;
+    let productAlreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item.id === product.id) {
+        productAlreadyInCart = true;
+        item.count++;
+      }
+    })
+    if(!productAlreadyInCart){
+      cartItems.push({...product, count:1, num: 1})
+    }
+    return cartItems;
+  })
+}
+
+increaseNumber = (item) => {
+  this.setState((state) => {
+    const cartItems = state.cartItems;
+    cartItems.forEach((ind) => {
+      if(ind.id === item.id && item.num >= 1) {
+        ind.num++;
+        console.log(ind.num)
+      }
+    })
+  })
+  // if () {
+  //   item.num = item.num + 1
+  //   console.log(item.num)
+  // }
+}
+
+decreaseNumber = (item) => {
+  if (item.num > 1) {
+    item.num = item.num - 1
+    console.log(item.num)
+  }
+}
+
   render() {
-    // console.log(this.state)
   return (
-    <>
+    <div className="women">
+    <div className="products">
     <Filter
     sort={this.state.sort}
     handleChangeCategory={this.handleChangeCategory}
     />
     <Products
-    allProducts={this.state.filteredProducts}
+    // allProducts={this.state.filteredProducts}
     handleAddToCart={this.handleAddToCart}
     />
-    </>
+    </div>
+    <Basket
+    increaseNumber={this.increaseNumber}  
+    decreaseNumber={this.decreaseNumber}
+    cartItems={this.state.cartItems}
+    handleRemoveFromCart={this.handleRemoveCart}
+    />
+    </div>
     );
   }
+}
 
-    // const { products } = this.props
-    
-  }
-    // <>
-    // {/* <Filter categories/>
-    // {/* <button onClick={() => categories[1] ? categories[1] : '' }>Clothes</button> */}
-    // {/* <section className="container">
-    //   {this.state.all.products.map((product) => (
-    // <article className="card" key={product.id}>
-    //   <div style={imageContainer}>
-    //   <img className="card_image" src={product.gallery[0]} alt={product.name} />
-    //     {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
-    //   </div>
-    //   <div>
-    //   <div>{product.name}</div>
-    //   <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
-    //   </div>
-    // </article>
-    //   ))}
-    // </section>
-    //   */}
-
-
-// const mapStateToProps = (state) => {
-//   return {
-//     cart: state.cart
-//   }
-// }
-
-// const mapDispatchToProps =(dispatch) => {
-//       return {
-//         Product: dispatch(addProduct)
-//         // categories: (product) => dispatch({ type: Action.ADD_PRODUCT, payload: product }),
-//       }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Women)
-
-
-// //   componentDidMount(){
-// // this.props.dispatch({
-//   payload: addProduct
-// })
-// }
-
-
-
-/* <div className="App">
-/* <Query query={getAllProducts}>
-{({ data }) => {
-        if (data) {
-          // destructuring data
-          const { products } = data.category;
-          // console.log(products)
-          return (
-            <>
-            <button onClick={() => this.props}>Change</button>
-            <section className="container">
-              {products.map((product) => (
-            <article className="card" key={product.id}>
-              <div style={imageContainer}>
-              <img className="card_image" src={product.gallery[0]} alt={product.name} />
-                {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
-              </div>
-              <div>
-              <div>{product.name}</div>
-              <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
-              </div>
-            </article>
-              ))}
-            </section>
-            </>
-          );
-        }
-      }}
-</Query>
-</div> */
