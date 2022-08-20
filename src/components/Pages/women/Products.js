ba1ad24cd2e4e51809b95ef5ee28819ddc365dbf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-20 01:07:38 +0100

    commented the increase and decrease functions

--- src/components/Pages/women/Products.js
+++ src/components/Pages/women/Products.js
@@ -10,27 +10,43 @@ class Products extends Component {
     this.props.fetchProducts()
   }
 
+  
+// increaseNumber = (item) => {
+//   if(item.num >= 1) {
+//     item.num++;
+//     console.log(item.num, item)
+//   }
+// }
+
+// decreaseNumber = (item) => {
+// if (item.num > 1) {
+// item.num = item.num - 1
+// console.log(item.num)
+//   }
+// }
+
   render() {
-    console.log(this.props.products)
-    const productItems = this.props.products.map(product => (
-            <article className="card" key={product.id}>
-                <div style={imageContainer}>
-                    <img className="card_image" src={product.gallery[0]} alt={product.name} />
-                    {product.inStock ? (<img src={addProductImage} alt={product.name}
-                    style={{cursor: "pointer"}}
-                    onClick={(e) => this.props.handleAddToCart(e, product)} />)
-                    : null}
-                    {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
-                </div>
-                <div>
-                    <div>{product.name}</div>
-                    <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
-                </div>
-            </article>
+    const productItems = this.props.products.items.map(product => (
+      <article className="card" key={product.id}>
+        <div style={imageContainer}>
+          <img className="card_image" src={product.gallery[0]} alt={product.name} />
+          {product.inStock ? (<img src={addProductImage} alt={product.name}
+          style={{cursor: "pointer"}}
+          onClick={(e) => this.props.handleAddToCart(e, product)} />)
+          : null}
+          {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
+        </div>
+        <div>
+          <div>{product.name}</div>
+          <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
+        </div>
+      </article>
     ))
 
     return (
-      <div>{productItems}</div>
+      <div>
+        {productItems}
+      </div>
     )
     }  
 }
