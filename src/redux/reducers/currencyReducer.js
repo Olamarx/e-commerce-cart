2da1f2b603b42ed97cdc079f4fbb80c5957c4b74
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-22 14:40:35 +0100

    added currencies reducer

---
+++ src/redux/reducers/currencyReducer.js
@@ -0,0 +1,12 @@
+import { FETCH_CURRENCIES } from "../action/types";
+
+const initialState = { currencies: [] }
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = initialState, action ) {
+    switch(action.type){
+        case FETCH_CURRENCIES:
+            return { ...state, currencies: action.payload }
+        default:
+            return state;
+    }
+}
