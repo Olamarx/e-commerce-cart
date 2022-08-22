0c261a67e20f0455d94c45caceb9df07617c03cc
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-22 14:39:21 +0100

    removed the console

--- src/redux/reducers/cartReducers.js
+++ src/redux/reducers/cartReducers.js
@@ -3,7 +3,6 @@ import { ADD_TO_CART } from "../action/types";
 
 const initialState = {items: [] }
 export default function (state = initialState, action) {
-    console.log(action.payload)
     switch (action.type) {
         case ADD_TO_CART:
             return { ...state, items: action.payload.cartItems }
