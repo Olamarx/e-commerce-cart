94dc210415686faef5f597332c846f6d4456718c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-22 14:38:43 +0100

    Added currencies action creator

---
+++ src/redux/action/currencies.js
@@ -0,0 +1,17 @@
+import { FETCH_CURRENCIES } from "./types";
+import { getCurrencies } from "../StoreReducer";
+
+export const fetchCurrencies = () => async (dispatch) => {
+    const response = await fetch("http://localhost:4000", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+                  "Accept": "application/json",
+              },
+              body: JSON.stringify({
+                  query: getCurrencies
+              })
+          });
+    const data = await response.json();
+    dispatch({type: FETCH_CURRENCIES, payload: data.data.currencies});
+}
