dde660bebd54e207885b5697a7688f9f381b2a99
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-22 12:28:53 +0100

    separated fetch and filter from add to cart

--- src/redux/action/types.js
+++ src/redux/action/types.js
@@ -1,3 +1,4 @@
 export const FETCH_PRODUCTS = "products/products/FETCH_PRODUCTS"
 export const FILTER_PRODUCTS = "filter/products/FILTER_PRODUCTS"
+
 export const ADD_TO_CART = "add/products/ADD_TO_CART"
