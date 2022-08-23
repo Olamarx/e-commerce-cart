8bd23888ff74b6a6a63eabf1dd557fd0e8d2dbb2
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-23 14:43:29 +0100

    Added navigation modula styling form

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Currencies from './Currencies';
 import logo from '../../utils/logo.png'
-
+import style from './Navigation.module.css'
 export default class Navigation extends Component {
   render() {
     const navLinkStyles = ({ isActive }) => ({
       fontWeight: isActive ? 'bold' : '300',
-      textDecoration: isActive ? 'underline' : 'none',
+      borderBottomWidth: isActive ? '1.5px' : '0px',
+      paddingBottom: '30px',
+      textDecoration: 'none',
+      width: '50px',
+      color: isActive ? '#5ECE7B' : 'black',
+      borderBottom: isActive ? '1px solid #5ECE7B' : 'none',
     });
     return (
       <>
       <header>
-        <nav>
-          <ul>
+        <nav className={style.nav}>
+          <ul className={style.link} >
             <li>
               <NavLink
                 to="/"
+                // className={style.link}
                 style={navLinkStyles}
               >
                   WOMEN
@@ -48,7 +54,6 @@ export default class Navigation extends Component {
 
           <div>
           <Currencies />
-            
           </div>
 
         </nav>
