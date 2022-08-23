4c10209a02f203f491e7ed6def77b15ac86199de
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-23 14:42:11 +0100

    Added currencies styling in modula form

--- src/components/Navigation/Currencies.js
+++ src/components/Navigation/Currencies.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCurrencies } from '../../redux/action/currencies';
+import img from '../../utils/Vector.png'
+import style from './Currencies.module.css'
+
 
 class Currencies extends Component {
 constructor(props){
@@ -18,26 +21,29 @@ handleChange = (event) => {
 
   render() {
     return (
-        <>
-        <select id="category" value={this.state.symbol}
+        <div className={style.select}>
+        <select id="category"
+        value={this.state.symbol}
         onChange={this.handleChange}
         >
-        <option>{this.state.symbol}</option>
+        <option >{this.state.symbol}</option>
           {
             this.props.currencies.map((each) => (
-              each.symbol === "A$" ? null : (
                 <option
                 key={each.symbol}
                 value={each.symbol}
                 >
                   {`${each.symbol} ${each.label}`}
                 </option>
-              )
             ))
           }
           
         </select>
-      </>
+        <div >
+          <img src={img} alt="cart" />
+          <span className={style.cart}>10</span>
+        </div>
+      </div>
     )
   }
 }
