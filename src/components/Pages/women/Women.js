edc8dcc38e6c69168e835196c7c139a84cbcc8fc
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:22:49 +0100

    Linter fixed an error

--- src/components/Pages/women/Women.js
+++ src/components/Pages/women/Women.js
@@ -1,49 +1,50 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import Filter from './Filter'
+import Filter from './Filter';
 import Basket from './Basket';
-import "./women.css"
+import './women.css';
+
 export default class Women extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { cartItems: [] }
+    this.state = { cartItems: [] };
   }
 
-handleAddToCart = (e, product) => {
-  this.setState((state) => {
-    const cartItems = state.cartItems;
-    let productAlreadyInCart = false;
-    cartItems.forEach((item) => {
-      if (item.id === product.id) {
-        productAlreadyInCart = true;
+  handleAddToCart = (e, product) => {
+    this.setState((state) => {
+      const { cartItems } = state;
+      let productAlreadyInCart = false;
+      cartItems.forEach((item) => {
+        if (item.id === product.id) {
+          productAlreadyInCart = true;
+        }
+      });
+      if (!productAlreadyInCart) {
+        cartItems.push({ ...product, num: 1 });
       }
-    })
-    if(!productAlreadyInCart){
-      cartItems.push({...product, num: 1})
-    }
-    return cartItems;
-  })
-}
+      return cartItems;
+    });
+  };
 
   render() {
-  return (
-    <div className="women">
-    <div className="products">
-    <Filter
-    sort={this.state.sort}
-    handleChangeCategory={this.handleChangeCategory}
-    />
-    <Products
-    allProducts={this.state.filteredProducts}
-    handleAddToCart={this.handleAddToCart}
-    />
-    </div>
-    <Basket
-    cartItems={this.state.cartItems}
-    handleRemoveFromCart={this.handleRemoveCart}
-    />
-    </div>
+    const { sort, filteredProducts, cartItems } = this.state;
+    return (
+      <div className="women">
+        <div className="products">
+          <Filter
+            sort={sort}
+            handleChangeCategory={this.handleChangeCategory}
+          />
+          <Products
+            allProducts={filteredProducts}
+            handleAddToCart={this.handleAddToCart}
+          />
+        </div>
+        <Basket
+          cartItems={cartItems}
+          handleRemoveFromCart={this.handleRemoveCart}
+        />
+      </div>
     );
   }
 }
-
