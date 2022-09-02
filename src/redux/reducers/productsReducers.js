9c526d3a2fb5263dbf30434ebb7ae93c4e1cc4cf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:28:22 +0100

    Linter fixed Single quote issue

--- src/redux/reducers/productsReducers.js
+++ src/redux/reducers/productsReducers.js
@@ -1,18 +1,18 @@
-import { FETCH_PRODUCTS, FILTER_PRODUCTS } from "../action/types";
+import { FETCH_PRODUCTS, FILTER_PRODUCTS } from '../action/types';
 
-const initialState = {items: [], filterItems: [], categoryName: ''}
+const initialState = { items: [], filterItems: [], categoryName: '' };
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (state = initialState, action ) {
-    switch(action.type){
-        case FETCH_PRODUCTS:
-            return {...state, items: action.payload, filterItems: action.payload}
-        case FILTER_PRODUCTS:
-            return {
-                ...state,
-                filterItems: action.payload.allProducts,
-                categoryName: action.payload.category
-            }
-        default:
-            return state;
-    }
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return { ...state, items: action.payload, filterItems: action.payload };
+    case FILTER_PRODUCTS:
+      return {
+        ...state,
+        filterItems: action.payload.allProducts,
+        categoryName: action.payload.category,
+      };
+    default:
+      return state;
+  }
 }
