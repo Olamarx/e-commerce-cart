10ed1cd89595a7d562cc3d5733dff92c14aed63a
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:21:33 +0100

    Eslint fix linter issue for the filter component

--- src/components/Pages/women/Filter.js
+++ src/components/Pages/women/Filter.js
@@ -1,31 +1,36 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { filterProducts } from '../../../redux/action/productActions'
-import './filter.css'
+import { filterProducts } from '../../../redux/action/productActions';
+import './filter.css';
 
 class Filter extends Component {
   render() {
     return (
       <>
-      <label className="label-container" htmlFor="category" >
-        Order by 
-        <select id="category" value={this.props.sort}
-        onChange={(e) => this.props.filterProducts(this.props.products, e.target.value)}
-        >
-          <option value="">All Products</option>
-          <option value="clothes">Clothes</option>
-          <option value="tech">Tech</option>
+        <label className="label-container" htmlFor="category">
+          Order by
+          <select
+            id="category"
+            value={this.props.sort}
+            onChange={(e) => this.props.filterProducts(this.props.products, e.target.value)}
+          >
+            <option value="">All Products</option>
+            <option value="clothes">Clothes</option>
+            <option value="tech">Tech</option>
 
-        </select>
-      </label>
+          </select>
+        </label>
       </>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   products: state.products.items,
-  category: state.products.categoryName
-})
+  category: state.products.categoryName,
+});
 
-export default connect(mapStateToProps, {filterProducts})(Filter)
+export default connect(mapStateToProps, { filterProducts })(Filter);
