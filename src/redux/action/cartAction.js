43297e31e9c634cffdca0737709c1028ea839887
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:25:16 +0100

    Linter fixed an error

--- src/redux/action/cartAction.js
+++ src/redux/action/cartAction.js
@@ -1,20 +1,21 @@
-import { ADD_TO_CART } from "./types";
+/* eslint-disable import/prefer-default-export */
+import { ADD_TO_CART } from './types';
 
 export const addToCart = (items, product) => (dispatch) => {
-    const cartItems = items.slice()
-    let productAlreadyInCart = false;
-    cartItems.forEach((item) => {
-          if (item.id === product.id) {
-            productAlreadyInCart = true;
-          }
-        })
-        if(!productAlreadyInCart){
-          cartItems.push({...product, num: 1})
-        }
-        return dispatch({
-            type: ADD_TO_CART,
-            payload: {
-                cartItems: cartItems
-            }
-        })
+  const cartItems = items.slice();
+  let productAlreadyInCart = false;
+  cartItems.forEach((item) => {
+    if (item.id === product.id) {
+      productAlreadyInCart = true;
     }
+  });
+  if (!productAlreadyInCart) {
+    cartItems.push({ ...product, num: 1 });
+  }
+  return dispatch({
+    type: ADD_TO_CART,
+    payload: {
+      cartItems,
+    },
+  });
+};
