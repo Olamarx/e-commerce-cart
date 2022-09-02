adc334f634aaa5539737cd1295fc7d84c2328012
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:16:21 +0100

    Fix linter issue

--- src/components/Navigation/Currencies.js
+++ src/components/Navigation/Currencies.js
@@ -1,53 +1,55 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-deprecated */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCurrencies } from '../../redux/action/currencies';
-import img from '../../utils/Vector.png'
-import style from './Currencies.module.css'
+import img from '../../utils/Vector.png';
+import style from './Currencies.module.css';
 import { addToCart } from '../../redux/action/cartAction';
 
 class Currencies extends Component {
-constructor(props){
-  super(props);
-  this.state = { symbol: "$" }
-}
-
-componentWillMount() {
-  this.props.fetchCurrencies()
-}
-
-handleChange = (event) => {
-  this.setState({ symbol: event.target.value });
-}
-
-result = () => {
-  const {cartItems} = this.props
-  let result = null
-  let check = 0
-  for(let i =0; i < cartItems.length; i++) {
-    check = cartItems[i].num + check
+  constructor(props) {
+    super(props);
+    this.state = { symbol: '$' };
   }
-  result = check
-  return result
-}
 
+  componentWillMount() {
+    this.props.fetchCurrencies();
+  }
+
+  handleChange = (event) => {
+    this.setState({ symbol: event.target.value });
+  };
+
+  result = () => {
+    const { cartItems } = this.props;
+    let result = null;
+    let check = 0;
+    for (let i = 0; i < cartItems.length; i += 1) {
+      check = cartItems[i].num + check;
+    }
+    result = check;
+    return result;
+  };
 
   render() {
-    console.log(this.result())
     return (
-        <div className={style.select}>
-        <select id="category"
-        value={this.state.symbol}
-        onChange={this.handleChange}
+      <div className={style.select}>
+        <select
+          id="category"
+          value={this.state.symbol}
+          onChange={this.handleChange}
         >
-        <option >{this.state.symbol}</option>
+          <option>{this.state.symbol}</option>
           {
             this.props.currencies.map((each) => (
-                <option
+              <option
                 key={each.symbol}
                 value={each.symbol}
-                >
-                  {`${each.symbol} ${each.label}`}
-                </option>
+              >
+                {`${each.symbol} ${each.label}`}
+              </option>
             ))
           }
         </select>
@@ -58,13 +60,13 @@ result = () => {
           }
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   currencies: state.currencies.currencies,
-  cartItems: state.cart.items
-})
+  cartItems: state.cart.items,
+});
 
-export default connect(mapStateToProps, {fetchCurrencies, addToCart})(Currencies)
+export default connect(mapStateToProps, { fetchCurrencies, addToCart })(Currencies);
