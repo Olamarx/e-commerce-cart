16b6b6f0dac92fb4bff1773bee70c4ab0e16d4e1
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:27:30 +0100

    Linter fixed Single quote issue

--- src/redux/reducers/currencyReducer.js
+++ src/redux/reducers/currencyReducer.js
@@ -1,12 +1,12 @@
-import { FETCH_CURRENCIES } from "../action/types";
+import { FETCH_CURRENCIES } from '../action/types';
 
-const initialState = { currencies: [] }
+const initialState = { currencies: [] };
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function (state = initialState, action ) {
-    switch(action.type){
-        case FETCH_CURRENCIES:
-            return { ...state, currencies: action.payload }
-        default:
-            return state;
-    }
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_CURRENCIES:
+      return { ...state, currencies: action.payload };
+    default:
+      return state;
+  }
 }
