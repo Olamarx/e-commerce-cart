a16a449b3c2c22cde3e13dad1e6aced39ce38cba
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 21:18:40 +0100

    Eslint fix linter issue of the kid file

--- src/components/Pages/Kids/Kids.js
+++ src/components/Pages/Kids/Kids.js
@@ -1,10 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
 export default class Kids extends Component {
   render() {
     return (
       <div>Kids</div>
-    )
+    );
   }
-};
-
+}
