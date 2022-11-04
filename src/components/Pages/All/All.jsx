0cfcb4ccf7323513dc42d712bdd9f0fe63c39550
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-04 10:32:15 +0100

    Renders all products but yet to style it

--- src/components/Pages/All/All.jsx
+++ src/components/Pages/All/All.jsx
@@ -1,36 +1,69 @@
-/* eslint-disable no-console */
-/* eslint-disable react/no-deprecated */
-/* eslint-disable import/extensions */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchCategories, fetchProducts } from '../../../redux/action/actionCreators';
+import { imageContainer, centered } from './helper';
+import addProductImage from '../../../utils/Common.png';
 import './women.css';
+import './loading.css';
 
 class All extends Component {
   componentDidMount() {
-    this.props.categories();
-    this.props.products();
+    this.props.fetchCategories();
+    this.props.fetchProducts();
   }
 
   render() {
-    console.log(this.props);
-    return (<div>Hell0</div>);
+    const { products } = this.props;
+
+    const allProducts = !products ? '' : products.products.map((product) => (
+      <article className="card" key={product.id}>
+        <div style={imageContainer}>
+          <img className="card_image1" src={product.gallery[0]} alt={product.name} />
+          {product.inStock ? (
+            <img
+              src={addProductImage}
+              alt={product.name}
+              style={{ cursor: 'pointer' }}
+            />
+          )
+            : null}
+          {!product.inStock ? (<div style={centered}>Out of stock</div>) : null}
+        </div>
+        <div>
+          <div>{product.name}</div>
+          <div>{`${product.prices[0].currency.symbol}${product.prices[0].amount}`}</div>
+        </div>
+      </article>
+    ));
+
+    return (
+      <>
+        { !products
+          ? (
+            <div className="ring">
+              Loading
+              <span />
+            </div>
+          )
+          : (
+            <div className="women">
+              <div className="products">
+                {allProducts}
+              </div>
+            </div>
+          ) }
+      </>
+    );
   }
 }
 
-const mapStateToProps = (state) => { 
-  console.log(state);
-   return {
-    products: state.products[0] 
-   }
-  };
+const mapStateToProps = (state) => ({ products: state.products[0] });
 
 export default connect(mapStateToProps,
   {
-    categories: fetchCategories,
-    products: fetchProducts,
+    fetchCategories,
+    fetchProducts,
   })(All);
