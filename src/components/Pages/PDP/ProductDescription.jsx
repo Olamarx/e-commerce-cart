32c724b665a688a52db2660df0b472429ba78112
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-13 20:42:20 +0100

    Finished the add to cart from PDPD

--- src/components/Pages/PDP/ProductDescription.jsx
+++ src/components/Pages/PDP/ProductDescription.jsx
@@ -3,70 +3,82 @@ import { connect } from 'react-redux';
 import { v4 as uuidv4 } from "uuid";
 import './options.css';
 
-import { Button, Price, Input, FormLike, AttriLI, AttriH3, MainBodyAttri, Main, AllImages, PDPBody, SmallImage, LeftImages } from './Style'
+import { Content, Button, Price, Input, FormLike, AttriLI, AttriH3, MainBodyAttri, Main, AllImages, PDPBody, SmallImage, LeftImages } from './Style'
 import { currencyPrice } from '../general/helper';
+import { addToCart } from '../../../redux/action/actionCreators';
+import Notification from './Notification';
 class ProductDescription extends Component {
   constructor(props) {
     super(props);
     this.state = {
       image: this.props.product.gallery[0],
-      isActive: false,
-        color: "",
-        size: "",
-        capacity: "",
-        wihtPorts: "",
-        wihtTouch: "",
-        setData: [],
-
-
+      set: false,
+      "Color": "",
+        "Size": "",
+        "Capacity": "",
+        "With USB 3 ports": "",
+        "Touch ID in keyboard": "",
     };    
   }
 
 
       setData = (e) => {
         const { name, value } = e.target;
-  
         if (name === "Size") {
-          this.setState({ size: value });
+          this.setState({ Size: value });
         } else if (name === "Color") {
           this.setState({ color: value });
         } else if (name === "Capacity") {
-          this.setState({ capacity: value });
+          this.setState({ Capacity: value });
         } else if (name === "With USB 3 ports") {
-          this.setState({ wihtPorts: value });
+          this.setState({ "With USB 3 ports": value });
         } else if (name === "Touch ID in keyboard") {
-          this.setState({ wihtTouch: value });
+          this.setState({ "Touch ID in keyboard": value });
         }
-
       };
       
-      handleData = (event) => {
-        console.log('working');
-        this.setState({ data: event.target.value })
-        console.log(this.state)
-        // console.log(event.target.value);
+      handleSubmit = (attributes, product, currency, addToCart) => {
+        const { state } = this
+        const data = [state["Touch ID in keyboard"], state['Size'], state['Capacity'], state["With USB 3 ports"], state["Color"]]
+        const filter = data.filter((datum) => datum !== "")
+        const attr =  {
+        "Color": state["Color"],
+        "Size": state['Size'],
+        "Capacity": state['Capacity'],
+        "With USB 3 ports": state["With USB 3 ports"],
+        "Touch ID in keyboard": state["Touch ID in keyboard"],
       }
-      
-      handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state.data1);
-        // console.log('data', this.state.data);
+        if (filter.length === attributes.length) {
+        console.log(filter);
+        addToCart({
+          ...product,
+          count: 1,
+          selectedAttr: attr,
+          selectedCurrency: currency,
+          currencyPrice: currencyPrice(currency, product.prices),
+          sum: 1 * parseFloat(currencyPrice(currency, product.prices)),
+        })
+        } else {
+          // this.setState({ set: true });
+          setTimeOut(() => this.setState({ set: true}), 3000)
+        }
       }
       
       
       render() {
         
         // const { attributes } = this.props
-
+        
 
         const handleSelectedImage = (img) => {
           // console.log(product)
           this.setState({image: img})
         };
-
-    const { product, currency } =  this.props;
+        const { set } = this.state
+    const { product, currency, addToCart } =  this.props;
     const { attributes, inStock } = product
     return (
+      <>
       <Main>
       <PDPBody>
         <AllImages>
@@ -87,7 +99,7 @@ class ProductDescription extends Component {
         </AllImages>
 
 <div>
-
+          {set ? (<Notification message="Kindly select out of the options" />) : null }
               {attributes && attributes.map((attribute) => (
               <MainBodyAttri key={attribute.name}>
                   <AttriH3>{attribute.name.toUpperCase()}:</AttriH3>
@@ -105,6 +117,7 @@ class ProductDescription extends Component {
                           name={attribute.name}
                           value={item.value}
                         />
+
                         <label 
                           className={
                             attribute.name === "Color" ? "label-color" : "label-no-color"
@@ -135,94 +148,25 @@ class ProductDescription extends Component {
 
                     <Button       
                  type="button"
-                    onClick={this.onSubmitProduct}
-                    disabled={!product.inStock && true}
+                    onClick={() => {
+                      this.handleSubmit(product.attributes, product, currency, addToCart)
+                      console.log(this.state);
+                      // console.log(this.state.setData);
+                    }}
+                    disabled={!inStock}
                   >
                     {!product.inStock ? "OUT OF STOCK" : "ADD TO CART"}
                   </Button>
+                  <Content dangerouslySetInnerHTML={{ __html: product.description }}/>
               </div>
-
-
-        {/* <RightContent> */}
-              {/* <h3>{product.name}</h3> */}
-{/* <form
-onSubmit={this.handleSubmit}
-style={{ display: 'flex', flexDirection: 'column', gap: '30px', }}
-> */}
-              
-              {/* <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Submit" /> */}
-
-          
-              {/* { product.attributes && product.attributes.map((attr) => ( */}
-
-                {/* <React.Fragment key={attr.id}>
-                <Ul>
-                {attr.items.length && attr.type !== 'swatch' ? (
-                  <>
-                  <h3>{attr.name}:</h3>
-                    { attr.items.map((item) => (
-                    <li key={item.id}>
-                    <input type="radio" id={item.id} value={this.state.data1} name={attr.id}
-                    onChange={(e) => this.setState({ data1: e.target.value })} required/>
-                    <label htmlFor={item.id}>{item.displayValue}</label>
-                    </li>
-                    )
-                    )
-                  }
-                  </>
-                  ) : 
-                  
-                  (<>
-                  <h3>{attr.name}:</h3>
-                
-                  { attr.items.map((color) => (
-                  <li key={color.id}>
-                    <input 
-                    value={item.displayValue}
-                    style={{backgroundColor: `${color.displayValue}`, width: '20px', height: '20px', }} type='radio' id={color.id} name={attr.id} required/>
-                    <label htmlFor={color.id}/>
-                </li>
-                  ))
-                }
-                </>)
-
-                  }
-                  </Ul>
-
-              </React.Fragment>
-              
-                )) }
-              <div>
-              { product.prices && (
-                <>
-                <h3>Price:</h3>
-                <input type='text'
-                style={{ border: 'none', outline: 'none', }}
-                value={`${currency} ${currencyPrice(currency, product.prices)}`} readOnly/>
-                </>
-              )
-              
-              }
-              </div>
-
-              <Button type='submit' value="ADD TO CART" />
-        </form>
-
-        <Content dangerouslySetInnerHTML={{ __html: product.description }}/>
-
-        </RightContent> */}
         </PDPBody>
 
       </Main>
+     </>
     )
   }
 }
 
 const mapStateToProps = (state) => ({ product: state.selected, currency: state.useCurrency });
 
-export default connect(mapStateToProps)(ProductDescription);
+export default connect(mapStateToProps, { addToCart })(ProductDescription);
