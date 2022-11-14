2a7d9f571461a5a0fa82544156c5c76896e28229
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-14 22:36:11 +0100

    Add a little refactoring

---
+++ src/components/Pages/PDP/PriceAndButton.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import { currencyPrice } from '../general/helper';
+import { AttriH3, Price, Button, Content } from './Style'
+
+class PriceAndButton extends Component {
+  render() {
+    const { currency, product, addToCart, handleSubmit } = this.props
+    const { inStock } = product
+    return (
+      <>
+                              <AttriH3>
+                      PRICE:
+                    </AttriH3>
+                    <Price>
+                      {`${currency} ${currencyPrice(currency, product.prices)}`}
+                    </Price>
+
+                    <Button       
+                 type="button"
+                    onClick={() => {
+                      handleSubmit(product.attributes, product, currency, addToCart)
+                    }}
+                    disabled={!inStock}
+                  >
+                    {!product.inStock ? "OUT OF STOCK" : "ADD TO CART"}
+                  </Button>
+                  <Content dangerouslySetInnerHTML={{ __html: product.description }}/>
+      </>
+    )
+  }
+}
+
+export default PriceAndButton;
