dca265f15a342703565601de562a428924abfe83
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-16 00:02:00 +0100

    Tried to check the data to be added to the cart

--- src/redux/reducers/addToCartReducer.js
+++ src/redux/reducers/addToCartReducer.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable no-fallthrough */
+import { checkData } from './helper';
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../action/types';
 
 const init = [];
 const addToCartReducer = (state = init, action) => {
+  // console.log(state);
   const newData = action.payload;
   switch (action.type) {
     case ADD_TO_CART:
-      return [...state, action.payload];
-      
+      checkData(state, action)
+
       case REMOVE_FROM_CART:
         console.log(newData)
         const filter = state.filter((product) => product.id !== newData.id);
