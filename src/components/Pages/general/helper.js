c7e5a4084c84cae667c54066c3eac0c29bd1155e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-28 20:28:46 +0100

    Yet to finish data comparison before sending to cart

--- src/components/Pages/general/helper.js
+++ src/components/Pages/general/helper.js
@@ -22,38 +22,60 @@ export const currencyPrice = (currency, array) => {
 
 export const addTheProduct  = (addToCart, products, product, currency, data) => {
   // I did this initial check for scalability reason
-  if (products.length){
-    console.log('Hello');
-    for (let i = 0; i < products.length; i++) {
-    if (product.attribute) {
-        for (let [key, value] of Object.entries(products[i].selectedAttr)) {
-          for (let [enteringKey, EnteringValue] of Object.entries(product.selectedAttr)) {
-              if (key === enteringKey && value === enteringKey) {
-              console.log('Send a message that it is already in the cart')
-              } 
-          }
-        }
-      } else if (product.id === products[i].id) {
-          console.log('Send a message that it is already in the cart')
-        } else {
-        addToCart({
-          ...product,
-          count: 1,
-          selectedAttr: data ? data : '',
-          selectedCurrency: currency,
-          currencyPrice: currencyPrice(currency, product.prices),
-          sum: 1 * parseFloat(currencyPrice(currency, product.prices)),
-      })
-}
-}
-} else {
-  addToCart({
+  const obj = {
     ...product,
     count: 1,
     selectedAttr: data ? data : '',
     selectedCurrency: currency,
     currencyPrice: currencyPrice(currency, product.prices),
     sum: 1 * parseFloat(currencyPrice(currency, product.prices)),
-  })
-} 
+}
+// let check = false
+  if (products.length){
+    const filter = products.filter((product) => product.id === obj.id);
+    for (let i = 0; i < filter.length; i++) {
+        // Because I have filtered it, I wont check it will have attributes and it would enhance the scalability
+        for (let [filterKey, filterVal] of Object.entries(filter[i].selectedAttr)) {
+        for (let [objKey, objVal] of Object.entries(obj.selectedAttr)) {
+            
+          // if (obj.selectedAttr && (obj.selectedAttr[key] !== filter[i].selectedAttr[key])) {
+          if (filterKey === objKey) {
+            console.log(filterKey, objKey, filterVal, objVal);
+            // check = true
+          }
+        }
+        }
+    }
+} else {
+  addToCart(obj)
+}
+
+//   if (products.length){
+//     // console.log('Hello');
+//     for (let i = 0; i < products.length; i++) {
+//     if (product.attributes && products[i].selectedAttr) {
+//         for (let [key, value] of Object.entries(products[i].selectedAttr)) {
+//           const getValue = products[i].selectedAttr[key]
+//           if (obj.selectedAttr[key] !== getValue) {
+//             // console.log(key);
+//             console.log('Send a message that it is already in the cart')
+//             // console.log(obj.selectedAttr[key], getValue);
+//             break;
+//             }
+//           // for (let [enteringKey, EnteringValue] of Object.entries(product.selectedAttr)) {
+//               // if (key === enteringKey && value === enteringKey) {
+//               // } 
+//           // }
+//         }
+//       } else if (product.id === products[i].id) {
+//           console.log('Send a message that product has the same id')
+//           break;
+//         } else {
+//         addToCart(obj)
+//         break;
+// }
+// }
+// } else {
+//   addToCart(obj)
+// } 
 }
