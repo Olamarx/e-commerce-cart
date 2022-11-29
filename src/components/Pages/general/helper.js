e6e7257456c573878fc3edba3236062f3dcaa9cf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-11-29 10:48:21 +0100

    Yet to finish data comparison before sending to cart again

--- src/components/Pages/general/helper.js
+++ src/components/Pages/general/helper.js
@@ -1,9 +1,10 @@
-export const imageContainer = {
+const imageContainer = {
   position: 'relative',
   textAlign: 'center',
   color: 'grey',
 };
-export const centered = {
+
+const centered = {
   position: 'absolute',
   fontSize: '30px',
   top: '50%',
@@ -11,16 +12,44 @@ export const centered = {
   transform: 'translate(-50%, -50%)',
 };
 
-export const currencyPrice = (currency, array) => {
+const currencyPrice = (currency, array) => {
   for (let i = 0; i < array.length; i += 1) {
     if (array[i].currency.symbol === currency) {
       return array[i].amount;
     }
   }
-  return null;
 };
 
-export const addTheProduct  = (addToCart, products, product, currency, data) => {
+// I would have used lodash to check the values but you said we should use less dependencies
+const isObject = (object) => {
+  return object != null && typeof object === 'object';
+}
+
+const isEqual = (enteringObj, cartObj) => {
+  let firstProp = Object.getOwnPropertyNames(enteringObj);
+  let secondProp = Object.getOwnPropertyNames(cartObj);
+  if (firstProp.length != secondProp.length) {
+    // console.log(false)
+      return false;
+  }
+  for (let i = 0; i < firstProp.length; i++) {
+      let val1 = enteringObj[firstProp[i]];
+      let val2 = cartObj[firstProp[i]];
+      let isObjects = isObject(val1) && isObject(val2);
+      if (isObjects && !isEqual(val1, val2) || !isObjects && val1 !== val2) {
+        // console.log(false)
+          return false;
+      }
+  }
+// console.log(true)
+
+  return true;
+}
+
+
+
+
+const addTheProduct  = (addToCart, products, product, currency, data) => {
   // I did this initial check for scalability reason
   const obj = {
     ...product,
@@ -30,52 +59,66 @@ export const addTheProduct  = (addToCart, products, product, currency, data) => 
     currencyPrice: currencyPrice(currency, product.prices),
     sum: 1 * parseFloat(currencyPrice(currency, product.prices)),
 }
-// let check = false
-  if (products.length){
-    const filter = products.filter((product) => product.id === obj.id);
-    for (let i = 0; i < filter.length; i++) {
-        // Because I have filtered it, I wont check it will have attributes and it would enhance the scalability
-        for (let [filterKey, filterVal] of Object.entries(filter[i].selectedAttr)) {
-        for (let [objKey, objVal] of Object.entries(obj.selectedAttr)) {
-            
-          // if (obj.selectedAttr && (obj.selectedAttr[key] !== filter[i].selectedAttr[key])) {
-          if (filterKey === objKey) {
-            console.log(filterKey, objKey, filterVal, objVal);
-            // check = true
-          }
-        }
-        }
-    }
-} else {
-  addToCart(obj)
+
+if (!products.length){
+  return addToCart(obj)
 }
 
-//   if (products.length){
-//     // console.log('Hello');
+const filter = products.filter((product) => product.id === obj.id);
+
+if (!filter.length) return addToCart(obj)
+
+
+// if  (products.length){
 //     for (let i = 0; i < products.length; i++) {
-//     if (product.attributes && products[i].selectedAttr) {
-//         for (let [key, value] of Object.entries(products[i].selectedAttr)) {
-//           const getValue = products[i].selectedAttr[key]
-//           if (obj.selectedAttr[key] !== getValue) {
-//             // console.log(key);
-//             console.log('Send a message that it is already in the cart')
-//             // console.log(obj.selectedAttr[key], getValue);
-//             break;
+   
+//       if (!product.attributes && (product.id !== products[i].id)) {
+//         console.log('Hello');
+//         return addToCart(obj)
+//       }
+
+//       const cartIndObjAttr = products[i].selectedAttr
+//       const enteringObjAttr = obj.selectedAttr
+   
+//       const isEqual = () => {
+//           let firstProp = Object.getOwnPropertyNames(enteringObjAttr);
+//           let secondProp = Object.getOwnPropertyNames(cartIndObjAttr);
+   
+//           if (firstProp.length !== secondProp.length) {
+//               return false;
+//           }
+//           for (let i = 0; i < firstProp.length; i++) {
+//               let val1 = enteringObjAttr[firstProp[i]];
+//               let val2 = cartIndObjAttr[firstProp[i]];
+//               let isObjects = isObject(val1) && isObject(val2);
+   
+//               if (isObjects && !isEqual() || !isObjects && val1 !== val2) {
+//                   return false;
+//               }
+   
 //             }
-//           // for (let [enteringKey, EnteringValue] of Object.entries(product.selectedAttr)) {
-//               // if (key === enteringKey && value === enteringKey) {
-//               // } 
-//           // }
+//         return addToCart(obj)
 //         }
-//       } else if (product.id === products[i].id) {
-//           console.log('Send a message that product has the same id')
-//           break;
-//         } else {
-//         addToCart(obj)
-//         break;
+//         isEqual()
+//       }
+//   } else {
+//       console.log('does not have any products before')
+//       return addToCart(obj)
+//     } 
+  }
 // }
+//  else {
+//   addTheProduct(obj)
 // }
-// } else {
-//   addToCart(obj)
-// } 
+   
+// }
+// Because I have filtered it, I wont check it will have attributes and it would enhance the scalability
+
+
+export {
+  imageContainer, 
+  centered, 
+  currencyPrice,
+  addTheProduct,
+  
 }
